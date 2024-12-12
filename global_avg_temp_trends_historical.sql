9747c92354baaa339559e771bee5c90351883e9e
Apoorv Singh <apoorvsinghlko@gmail.com>
2024-12-12 14:46:47 -0500

    Add files via upload

--- global_avg_temp_trends_historical.sql
+++ global_avg_temp_trends_historical.sql
@@ -1,16 +1,33 @@
 USE hive.as17979_nyu_edu;
 
 SELECT 
-  city, 
-  country, 
-  ROUND(AVG(averagetemperature), 3) AS Avg_Temp_1993_2013
-FROM city
-WHERE (
-  (REGEXP_LIKE(dt, '^\d{4}-\d{2}-\d{2}$') AND CAST(SUBSTR(dt, 1, 4) AS INT) BETWEEN 1993 AND 2013) OR
-  (REGEXP_LIKE(dt, '^\d{1,2}/\d{1,2}/\d{4}$') AND LENGTH(SPLIT_PART(dt, '/', 3)) = 4 AND CAST(SPLIT_PART(dt, '/', 3) AS INT) BETWEEN 1993 AND 2013)
-)
-GROUP BY city, country
-ORDER BY city;
+  year, 
+  ROUND(AVG(averagetemperature), 3) AS Global_Avg_Temperature
+FROM (
+  SELECT 
+    CAST(
+      CASE
+        WHEN REGEXP_LIKE(dt, '^\d{4}-\d{2}-\d{2}$') THEN SUBSTR(dt, 1, 4)
+        WHEN REGEXP_LIKE(dt, '^\d{1,2}/\d{1,2}/\d{4}$') THEN SPLIT_PART(dt, '/', 3)
+        ELSE NULL
+      END AS INTEGER
+    ) AS year, 
+    city, 
+    averagetemperature
+  FROM city
+  WHERE 
+    (REGEXP_LIKE(dt, '^\d{4}-\d{2}-\d{2}$') OR REGEXP_LIKE(dt, '^\d{1,2}/\d{1,2}/\d{4}$')) 
+    AND CAST(
+      CASE
+        WHEN REGEXP_LIKE(dt, '^\d{4}-\d{2}-\d{2}$') THEN SUBSTR(dt, 1, 4)
+        WHEN REGEXP_LIKE(dt, '^\d{1,2}/\d{1,2}/\d{4}$') THEN SPLIT_PART(dt, '/', 3)
+        ELSE NULL
+      END AS INTEGER
+    ) BETWEEN 1849 AND 2013
+) yearly_avg
+GROUP BY year
+ORDER BY year;
+
 
 
 -- Run using "trino --output-format CSV -f global_avg_temp_trends_historical.sql > results_global_avg.csv"
--- predicted_temp_for_next_decade.sql
+++ predicted_temp_for_next_decade.sql
@@ -1,32 +1,33 @@
 USE hive.as17979_nyu_edu;
 
 SELECT 
-  year, 
-  ROUND(AVG(averagetemperature), 3) AS Global_Avg_Temperature
+  md.city, 
+  md.country, 
+  ROUND(AVG(regress(features(CAST(fy.year AS DOUBLE)), md.model)), 3) AS Avg_Temp_Next_Decade
 FROM (
   SELECT 
-    CAST(
-      CASE
-        WHEN REGEXP_LIKE(dt, '^\d{4}-\d{2}-\d{2}$') THEN SUBSTR(dt, 1, 4)
-        WHEN REGEXP_LIKE(dt, '^\d{1,2}/\d{1,2}/\d{4}$') THEN SPLIT_PART(dt, '/', 3)
-        ELSE NULL
-      END AS INTEGER
-    ) AS year, 
     city, 
-    averagetemperature
+    country, 
+    learn_regressor(
+      averagetemperature, 
+      features(CAST(
+        CASE
+          WHEN REGEXP_LIKE(dt, '^\d{4}-\d{2}-\d{2}$') THEN SUBSTR(dt, 1, 4)
+          WHEN REGEXP_LIKE(dt, '^\d{1,2}/\d{1,2}/\d{4}$') THEN CAST(SPLIT_PART(dt, '/', 3) AS VARCHAR)
+          ELSE NULL
+        END AS DOUBLE))
+    ) AS model
   FROM city
-  WHERE 
-    (REGEXP_LIKE(dt, '^\d{4}-\d{2}-\d{2}$') OR REGEXP_LIKE(dt, '^\d{1,2}/\d{1,2}/\d{4}$')) 
-    AND CAST(
-      CASE
-        WHEN REGEXP_LIKE(dt, '^\d{4}-\d{2}-\d{2}$') THEN SUBSTR(dt, 1, 4)
-        WHEN REGEXP_LIKE(dt, '^\d{1,2}/\d{1,2}/\d{4}$') THEN SPLIT_PART(dt, '/', 3)
-        ELSE NULL
-      END AS INTEGER
-    ) BETWEEN 1849 AND 2013
-) yearly_avg
-GROUP BY year
-ORDER BY year;
+  WHERE REGEXP_LIKE(dt, '^\d{4}-\d{2}-\d{2}$') OR REGEXP_LIKE(dt, '^\d{1,2}/\d{1,2}/\d{4}$')
+  GROUP BY city, country
+) md
+CROSS JOIN (
+  SELECT year 
+  FROM UNNEST(SEQUENCE(2025, 2035)) AS t(year)
+) fy
+GROUP BY md.city, md.country
+ORDER BY md.city;
+
 
 
 -- Run using "trino --output-format CSV -f predicted_temp_for_next_decade.sql > results_predicted_temp.csv"
